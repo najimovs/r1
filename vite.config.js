70176373cd366336d52edaa585eaf13321f0aaeb
Muhammadxon Najimov <129692531+najimovs@users.noreply.github.com>
2025-08-25 11:55:22 +0500

    Add Tailwind

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from "path"
+import tailwindcss from "@tailwindcss/vite"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
 
 export default defineConfig( {
 	server: {
 		host: true,
 	},
-	plugins: [ react() ],
+	plugins: [
+		react(),
+		tailwindcss(),
+	],
+	resolve: {
+		alias: {
+			"@": path.resolve( __dirname, "./src" ),
+		},
+	},
 } )
