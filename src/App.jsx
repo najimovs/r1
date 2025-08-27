cc69fb08dd14491848f2f00e10f108df19b3d4ec
Muhammadxon Najimov <129692531+najimovs@users.noreply.github.com>
2025-08-27 10:33:57 +0500

    Cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,6 @@ const Error404 = lazy( () => import( "@/pages/Error404" ) )
 
 export function App() {
 
-	const result1 = add( 2, 3 )
-	console.log( result1 )
-
-	const result2 = add( 2, 3 )
-	console.log( result2 )
-
 	return (
 		<Suspense fallback={ <>loading...</> }>
 			<Routes>
@@ -23,28 +17,4 @@ export function App() {
 			</Routes>
 		</Suspense>
 	)
-}
-
-const memo = new Map()
-
-function add( a, b ) {
-
-	const key = `${ a }_${ b }`
-
-	let result = null
-
-	if ( memo.has( key ) ) {
-
-		result = memo.get( key )
-
-		console.info( "from cache" )
-	}
-	else {
-
-		result = a + b
-
-		memo.set( key, result )
-	}
-
-	return result
 }
